5cd54ebb5e5d2e3866ef4c5f22d842ef3c45be5a
Amaury <amaury24120606@gmail.com>
2025-09-15 13:51:30 +0200

    Ajout des fonctionnalités de comptage de ligne et de mots

--- exercice-5/main.py
+++ exercice-5/main.py
@@ -1,11 +1,20 @@
 def charger_fichier(nomfichier):
     nomdufichier = nomfichier
+    nblignes = 0
+    nbmots = 0
     try:
         with open(f"exercice-5/{nomdufichier}","r") as fichier:
             for ligne in fichier:
+                ligne = ligne.strip()
+                nblignes += 1
                 print(ligne)
-            for mots in fichier:
+
+                mots = ligne.split()
+                nbmots += len(mots)
                 print(mots)
+                nbmots +=1
+            print(f"Il y a {nbmots} mots dans ce texte.")
+            print(f"Il y a {nblignes} lignes dans ce texte.")
     except FileNotFoundError:
         pass
     return fichier
